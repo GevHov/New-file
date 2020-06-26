689bdf79154a76a042c38ccfc1ccdf869b466fb7
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-06-26 19:35:42 +0400

    New-CommitGit

---
+++ NewGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewGit
+//
+//  Created by MacBook Pro on 6/26/20.
+//  Copyright © 2020 Gevorg Hovhannisyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello Vars")
+    }
+
+
+}
+
